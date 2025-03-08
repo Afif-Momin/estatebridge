30ff2358cca686308b5048de88c378212123fca9
Afif-Momin <118838171+Afif-Momin@users.noreply.github.com>
2025-03-08 16:44:53 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,1 +1,7 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("config.json")  # Your Firebase service account key
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
